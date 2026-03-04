bc80b11d1495443de055cb4d9a2ab733d16e4b1f
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-05 00:03:37 +0800

    Update project

--- dev_board.js
+++ dev_board.js
@@ -1,30 +1,33 @@
-document.getElementById('cmplt-1-btn')
-    .addEventListener('click', function () {
+const btnEl = document.querySelector('button')
 
-        
+document.getElementById('button')
+btnEl.addEventListener('click', () => {
+    alert("hello world")
 
-        const pointNumber5 = document.getElementById('point-number-5').innerText;
-        const pointNumber23 = document.getElementById('point-number-23').innerText;
-        // console.log(typeof pointNumber23);
+    btnEl.disabled = true;
 
-        // converted string to number 
-        const convertedPointNumber5 = parseInt(pointNumber5);
-        const convertedPointNumber23 = parseInt(pointNumber23);
+    const pointNumber5 = document.getElementById('point-number-5').innerText;
+    const pointNumber23 = document.getElementById('point-number-23').innerText;
+    // console.log(typeof pointNumber23);
 
-        if (convertedPointNumber5 > 0) {
-            const sum = convertedPointNumber5 - 1;
-            const sum2 = convertedPointNumber23 + 1;
-            document.getElementById('point-number-5').innerText = sum;
-            document.getElementById('point-number-23').innerText = sum2;
+    // converted string to number 
+    const convertedPointNumber5 = parseInt(pointNumber5);
+    const convertedPointNumber23 = parseInt(pointNumber23);
 
-        }
-        else {
-            alert('not count your number');
-        }
+    if (convertedPointNumber5 > 0) {
+        const sum = convertedPointNumber5 - 1;
+        const sum2 = convertedPointNumber23 + 1;
+        document.getElementById('point-number-5').innerText = sum;
+        document.getElementById('point-number-23').innerText = sum2;
 
-
-
-
+    }
+    else {
+        alert('not count your number');
+    }
 
 
-    });
+
+
+
+
+});
