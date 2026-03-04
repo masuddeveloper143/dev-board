29bf6180060de1a7d54c9cc347de6128fee9e37e
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-04 16:45:43 +0800

    Update project

--- dev_board.js
+++ dev_board.js
@@ -11,8 +11,17 @@ document.getElementById('cmplt-1-btn')
         const convertedPointNumber5 = parseInt(pointNumber5);
         const convertedPointNumber23 = parseInt(pointNumber23);
 
-        if (convertedPointNumber5 > 0){
-            
+        if (convertedPointNumber5 > 0) {
+            const sum = convertedPointNumber5 -= 1;
+            const sum2 = convertedPointNumber23 += 1;
+            document.getElementById('point-number-5', sum);
+            document.getElementById('point-number-23', sum2);
         }
+
+        else{
+            alert('not count your number');
+        }
+
+    
 
     })
