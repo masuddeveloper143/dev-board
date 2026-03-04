3a77cefa8400c37fee7eb8261a8e20bba38c8227
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-04 16:34:57 +0800

    Update project

---
+++ dev_board.js
@@ -0,0 +1,11 @@
+document.getElementById('cmplt-1-btn')
+    .addEventListener('click', function () {
+
+
+
+        const pointNumber5 = document.getElementById('').innerText;
+        const pointNumber23 = document.getElementById('').innerText;
+
+        const convertedPointNumber5 = parseInt('');
+        const convertedPointNumber23 = parseInt('');
+    })
